b9edb71728eac54e07c7422cd2e89348b8d5fd36
CarlaBrtl <carla.brtl@gmail.com>
2019-02-05 19:28:55 +0000

    Try and view the images of the data_sample folder

--- model.py
+++ model.py
@@ -1,4 +1,23 @@
 # Load the data 
+import csv
+import cv2
+import matplotlib.pyplot as plt
+
+lines = []
+# with open('../data_sample/driving_log.csv') as csvfile: 
+with open('data_sample/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader: 
+        lines.append(line) 
+        
+images = []
+measurements = []
+for line in lines: 
+    source_path = line[0]
+    image = cv2.imread(source_path)
+    
+    plt.imshow(image) 
+    plt.show()
 # Shuffle the data, use 70% for training, and 30% for validation 
 # Preprocess the image
 # Create the model 
