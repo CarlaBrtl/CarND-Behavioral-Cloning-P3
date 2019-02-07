968adcb189fa71231555706f4184afd5f334af92
CarlaBrtl <carla.brtl@gmail.com>
2019-02-07 10:10:46 +0000

    Get the data and organize into a training and validation data set

--- model.py
+++ model.py
@@ -1,25 +1,43 @@
 # Load the data 
 import csv
 import cv2
-import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
 
-lines = []
-# with open('../data_sample/driving_log.csv') as csvfile: 
-with open('data_sample/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader: 
-        lines.append(line) 
+def get_data(): 
+    lines = [] 
+    # with open('../data_sample/driving_log.csv') as csvfile: 
+    with open('data_sample/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader: 
+            lines.append(line) 
         
-images = []
-measurements = []
-for line in lines: 
-    source_path = line[0]
-    image = cv2.imread(source_path)
-    
-    plt.imshow(image) 
-    plt.show()
+    images = [] ## images = x in our architecture
+    measurements = [] ## measurements = y in our architecture
+    for line in lines: 
+        source_path = line[0]
+        image = cv2.imread(source_path)
+        images.append(image)
+        steering_angle = line[6]
+        measurements.append(steering_angle) 
+        
+    print('Total number of images loaded: ' + str(len(images)))
+    return images, measurements
+
 # Shuffle the data, use 70% for training, and 30% for validation 
+def separate_training_and_validation_data(X_data, y_data, training_percent = 70): 
+    X_data, y_data = shuffle(X_data, y_data)
+    
+    training_len = int(len(X_data) * training_percent / 100)
+    
+    X_train, y_train, X_valid, y_valid = X_data[:training_len], y_data[:training_len], X_data[training_len:], y_data[training_len:]
+    print('Size of training set: ' + str(len(X_train)))
+    print('Size of validation set: ' + str(len(X_valid)))
+    return X_train, y_train, X_valid, y_valid
+
 # Preprocess the image
 # Create the model 
 # Train the model, and evaluate
 # Run the 
+X_data, y_data = get_data()
+X_train, y_train, X_valid, y_valid = separate_training_and_validation_data(X_data, y_data) 
+ 
