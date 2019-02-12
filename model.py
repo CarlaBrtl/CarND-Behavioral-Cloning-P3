23f09190e99151fc18c590a8bc21d70bbe8073f2
CarlaBrtl <carla.brtl@gmail.com>
2019-02-12 18:11:12 +0000

    Fix generator

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 from keras.applications.inception_v3 import InceptionV3
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
@@ -13,16 +14,15 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
 from keras.layers.core import Dense, Activation, Flatten, Dropout
 from keras.layers import GlobalAveragePooling2D, Input, Lambda, Cropping2D
-from sklearn.model_selection import train_test_split
 
 def get_image_names(): 
     lines = [] 
     # with open('../data_sample/driving_log.csv') as csvfile: 
     with open('data/driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
-        for line in reader: 
+        for line in reader:
             lines.append(line)
-   return lines         
+    return lines         
             
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -34,7 +34,7 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
+                name = 'data/IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
@@ -43,7 +43,7 @@ def generator(samples, batch_size=32):
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
+            yield shuffle(X_train, y_train)
 
 ## TODO Change this
 def plot_measurements(measurements):
@@ -76,26 +76,33 @@ def data_generator(X_train, y_train):
     for i in range(len(X_train)):
         yield X_train[i], y_train[i]
     
-def train(train_samples, validation_samples, batch_size=32, epochs=2): 
+def train(train_generator, train_samples, validation_generator, validation_samples, epochs=2): 
     print('Training')
     model = get_model()
 
     model.compile(loss = 'mse' , optimizer='adam')
     
-    model.fit_generator(traincd_generator, steps_per_epoch=len(X_train)/batch_size, epochs=epochs, verbose=1, 
-                    validation_data=validation_generator,
-                    validation_steps=len(X_val)/batch_size)
+    model.fit_generator(train_generator, samples_per_epoch=len(train_samples), 
+                        validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=epochs)
     
     model.save('model.h5')
     return model 
 
-X_data_names = get_image_names
-
-# Shuffle the data, use 70% for training, and 30% for validation 
-X_data_names_train , X_data_names_valid = train_test_split(X_data_names, test_size=0.3)
+samples = get_image_names()
+# Shuffle the data, use 80% for training, and 20% for validation 
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-model = train(train_generator, validation_generator)
+# Get and compile mode 
+model = get_model()
+model.compile(loss='mse', optimizer='adam')
+
+# Train the model
+print("Training")
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), 
+                    validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+
+model.save('model.h5')
 
